5e1bda338bc2858c3062ca0c935d35386ac026ef
Onur Ozgur OZKAN <onur.ozgur.ozkan@bulutfon.com>
2024-01-02 05:11:17 +0000

    Initial commit.

---
+++ lib/hipcall_disposable_email.ex
@@ -0,0 +1,18 @@
+defmodule HipcallDisposableEmail do
+  @moduledoc """
+  Documentation for `HipcallDisposableEmail`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HipcallDisposableEmail.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hipcall_disposable_email_test.exs
@@ -0,0 +1,8 @@
+defmodule HipcallDisposableEmailTest do
+  use ExUnit.Case
+  doctest HipcallDisposableEmail
+
+  test "greets the world" do
+    assert HipcallDisposableEmail.hello() == :world
+  end
+end
